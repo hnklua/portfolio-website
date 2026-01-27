7ef307cc0213ec4e8d50e8e48f038d4a35e8d26e
COhiri <charles.ohiri@wsu.edu>
2026-01-26 20:59:08 -0800

    Initial Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, 
+  },
+  trailingSlash: true, 
   reactCompiler: true,
 };
 
 export default nextConfig;
+ 
